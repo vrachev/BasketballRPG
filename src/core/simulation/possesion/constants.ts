920fbc8ec1d760607952459bcf65cc8069bae0fa
vrachev <vrachev96@gmail.com>
2024-10-24 19:53:06 -0400

    update league average probabilities

--- src/core/simulation/possesion/constants.ts
+++ src/core/simulation/possesion/constants.ts
@@ -1,4 +1,9 @@
-// https://shottracker.com/articles/the-3-point-revolution#:~:text=Five%20years%20after%20its%20inception,the%20trend%20is%20leveling%20off.
+/**
+ *  Stats from:
+ *  https://shottracker.com/articles/the-3-point-revolution#:~:text=Five%20years%20after%20its%20inception,the%20trend%20is%20leveling%20off.
+ * 
+ * https://www.basketball-reference.com/leagues/NBA_stats_per_game.html
+ * */
 
 export const possessionConstants = {
   shotClock: 24,
@@ -11,38 +16,38 @@ export const playerConstants = {
 };
 
 export const averageGameStatsPerTeam = {
-  possessions: 99.1,
-  FreeThrowPercentage: 0.784,
-  FreeThrowAttempts: 21.7,
-  fga: 88.9,
+  possessions: 100.0,
+  FreeThrowPercentage: 0.763,
+  FreeThrowAttempts: 27.3,
+  fga: 88.6,
   twoPointShot: {
-    rimRate: 0.32,
+    rimRate: 0.305,
     rimPercentage: 0.66,
-    paintRate: 0.16,
+    paintRate: 0.135,
     paintPercentage: 0.435,
-    midRangeRate: 0.19,
+    midRangeRate: 0.16,
     midRangePercentage: 0.435,
     get twoPointShotRate() {
       return this.rimRate + this.paintRate + this.midRangeRate;
     },
   },
   threePointShot: {
-    cornerThreeRate: 0.07,
+    cornerThreeRate: 0.09,
     cornerThreePercentage: 0.4,
-    aboveTheBreakThreeRate: 0.26,
+    aboveTheBreakThreeRate: 0.31,
     aboveTheBreakThreePercentage: 0.37,
     get threePointShotRate() {
       return this.cornerThreeRate + this.aboveTheBreakThreeRate;
     },
   },
   assistPercentage: 0.55,
-  steals: 7.5,
-  blocks: 5.1,
-  turnovers: 13.6,
-  offensiveRebounds: 10.6,
-  defensiveRebounds: 33.0,
+  steals: 7.7,
+  blocks: 5.9,
+  turnovers: 14.7,
+  offensiveRebounds: 11.6,
+  defensiveRebounds: 32.9,
   get rebounds() {
     return this.offensiveRebounds + this.defensiveRebounds;
   },
-  personalFouls: 18.7,
+  personalFouls: 22.3,
 };
